d960b8add7269740f939fee2306d029aabc6f178
coderladfahim <fahimalemroz@gmail.com>
2022-12-29 09:46:04 +0600

    Fixed typo

--- init.lua
+++ init.lua
@@ -10,5 +10,5 @@ local transparent_cmd_1 = 'highlight Normal guibg=none';
 local transparent_cmd_2 = 'highlight NonText guibg=none';
 
 vim.cmd(colorscheme_command)
-vim.cmd(transparent_cmd_1c
+vim.cmd(transparent_cmd_1)
 vim.cmd(transparent_cmd_2)
