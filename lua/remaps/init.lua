ae32dcf874d86ddcaa5f5be775f23fa1cad4cbb1
emroz <fahimalemroz@gmail.com>
2023-02-10 17:25:28 +0600

    Added better remaps for split sizing.

--- lua/remaps/init.lua
+++ lua/remaps/init.lua
@@ -1,64 +1,63 @@
 local opts = { noremap = true, silent = false }
-local keymap = vim.api.nvim_set_keymap
 
-keymap('n', '<leader>w', ':w<CR>', opts)
-keymap('n', '<leader>q', ':q!', opts)
-keymap('n', '<leader>src', ':luafile %', opts)
-keymap('n', '<leader>v', '<C-v>', opts)
-keymap('n', '<leader>fv', ':file<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>q', ':q!', opts)
+vim.api.nvim_set_keymap('n', '<leader>src', ':luafile %', opts)
+vim.api.nvim_set_keymap('n', '<leader>v', '<C-v>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fv', ':file<cr>', opts)
 
 -- Better tab controls
-keymap('n', '<leader>tn', ':tabedit<cr>', opts)
-keymap('n', '<leader>tq', ':tabclose!<cr>', opts)
-keymap('n', '<leader>taq', ':tabonly!<cr>', opts)
-keymap('n', '}', ':BufferLineCycleNext<cr>', opts)
-keymap('n', '{', ':BufferLineCyclePrev<cr>', opts)
-keymap('n', '<leader>bp', ':BufferLinePick<cr>', opts)
-keymap('n', '<leader>bq', ':b#|bd# <cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tn', ':tabedit<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tq', ':tabclose!<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>taq', ':tabonly!<cr>', opts)
+vim.api.nvim_set_keymap('n', '}', ':BufferLineCycleNext<cr>', opts)
+vim.api.nvim_set_keymap('n', '{', ':BufferLineCyclePrev<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>bp', ':BufferLinePick<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>bq', ':b#|bd# <cr>', opts)
 
-keymap('n', '<leader>fr', ':%s/', opts)
+vim.api.nvim_set_keymap('n', '<leader>fr', ':%s/', opts)
 
 -- Packer controls
-keymap('n', '<leader>pc', '<cmd>PackerClean<cr>', opts)
-keymap('n', '<leader>pc', '<cmd>PackerInstall<cr>', opts)
-keymap('n', '<leader>ps', '<cmd>PackerSync<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>pc', '<cmd>PackerClean<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>pc', '<cmd>PackerInstall<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>ps', '<cmd>PackerSync<cr>', opts)
 
-keymap('n', "<leader>hh","<C-w>h", opts)
-keymap('n', "<leader>jj","<C-w>j", opts)
-keymap('n', "<leader>kk","<C-w>k", opts)
-keymap('n', "<leader>ll","<C-w>l", opts)
+vim.api.nvim_set_keymap('n', "<leader>hh","<C-w>h", opts)
+vim.api.nvim_set_keymap('n', "<leader>jj","<C-w>j", opts)
+vim.api.nvim_set_keymap('n', "<leader>kk","<C-w>k", opts)
+vim.api.nvim_set_keymap('n', "<leader>ll","<C-w>l", opts)
 
-keymap('n', "<leader>HH", "<cmd>vertical resize +2<CR>", opts)
-keymap('n', "<leader>JJ", "<cmd>resize +2<CR>", opts)
-keymap('n', "<leader>KK", "<cmd>resize -2<CR>", opts)
-keymap('n', "<leader>LL", "<cmd>vertical resize -2<CR>", opts)
+vim.api.nvim_set_keymap('n', "(", "<cmd>vertical resize +2<CR>", opts)
+vim.api.nvim_set_keymap('n', "+", "<cmd>resize +2<CR>", opts)
+vim.api.nvim_set_keymap('n', "-", "<cmd>resize -2<CR>", opts)
+vim.api.nvim_set_keymap('n', ")", "<cmd>vertical resize -2<CR>", opts)
 
 -- Nvim tree mappings
-keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', opts)
-keymap('n', '<leader>tf', ':NvimTreeFindFile<CR>', opts)
-keymap('n', '<leader>fc', ':NvimTreeCollapse<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tf', ':NvimTreeFindFile<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fc', ':NvimTreeCollapse<CR>', opts)
 
 -- Telescope mappings
-keymap('n', "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
-keymap('n', "<leader>fg", "<cmd>Telescope git_files<CR>", opts)
-keymap('n', "<leader>fw", "<cmd>Telescope live_grep<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>fg", "<cmd>Telescope git_files<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>fw", "<cmd>Telescope live_grep<CR>", opts)
 
 -- Toggleterm mappings
-keymap('n', "<leader>lg", "<cmd>lua LAZYGIT_TOGGLE()<CR>", opts)
-keymap('n', "<leader>nd", "<cmd>lua NODE_TOGGLE()<CR>", opts)
-keymap('n', "<leader>tk", "<cmd>lua ARTISAN_TOGGLE()<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>lg", "<cmd>lua LAZYGIT_TOGGLE()<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>nd", "<cmd>lua NODE_TOGGLE()<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>tk", "<cmd>lua ARTISAN_TOGGLE()<CR>", opts)
 
 
 -- git mappings
-keymap('n', "<leader>ga.", ":G add .", opts)
-keymap('n', "<leader>gs", ":G status", opts)
-keymap('n', "<leader>glo", ":G log --oneline ", opts)
-keymap('n', "<leader>gcm", ':G commit -m ""', opts)
-keymap('n', "<leader>gcam", ':G commit --amend -m ""', opts)
-keymap('n', "<leader>gcan", ':G commit --amend --no-edit', opts)
-keymap('n', "<leader>gr.", ":G restore .", opts)
+vim.api.nvim_set_keymap('n', "<leader>ga.", ":G add .", opts)
+vim.api.nvim_set_keymap('n', "<leader>gs", ":G status", opts)
+vim.api.nvim_set_keymap('n', "<leader>glo", ":G log --oneline ", opts)
+vim.api.nvim_set_keymap('n', "<leader>gcm", ':G commit -m ""', opts)
+vim.api.nvim_set_keymap('n', "<leader>gcam", ':G commit --amend -m ""', opts)
+vim.api.nvim_set_keymap('n', "<leader>gcan", ':G commit --amend --no-edit', opts)
+vim.api.nvim_set_keymap('n', "<leader>gr.", ":G restore .", opts)
 
-keymap('n', "<C-d>", "<C-d>zz", opts)
-keymap('n', "<C-u>", "<C-u>zz", opts)
-keymap('n', "<C-f>", "<C-f>zz", opts)
-keymap('n', "<C-b>", "<C-b>zz", opts)
+vim.api.nvim_set_keymap('n', "<C-d>", "<C-d>zz", opts)
+vim.api.nvim_set_keymap('n', "<C-u>", "<C-u>zz", opts)
+vim.api.nvim_set_keymap('n', "<C-f>", "<C-f>zz", opts)
+vim.api.nvim_set_keymap('n', "<C-b>", "<C-b>zz", opts)
