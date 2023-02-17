34a12e669ea8da981777f4ed38d737bc2d9dcedf
emroz <fahimalemroz@gmail.com>
2023-02-17 22:09:50 +0600

    Update

--- init.lua
+++ init.lua
@@ -7,10 +7,9 @@ require('lsp');
 
 function SET_COLORSCHEME()
 	local colorscheme_command = 'colo sonokai';
-	local transparent_cmd_1 = 'highlight Normal guibg=none';
-	local transparent_cmd_2 = 'highlight NonText guibg=none';
+	-- local transparent_cmd_1 = 'highlight Normal guibg=none';
+	-- local transparent_cmd_2 = 'highlight NonText guibg=none';
 
-	vim.cmd(colorscheme_command)
 	vim.cmd(colorscheme_command)
 	-- vim.cmd(transparent_cmd_1)
 	-- vim.cmd(transparent_cmd_2)
