8765f32f499a791d35de2037205f635026c572c3
Emrozgs <emroz@gain.media>
2023-04-05 11:52:21 +0600

    Foreground change

--- lua/globals.lua
+++ lua/globals.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = ' ' -- mapping the leader key to be space
 vim.g.loaded_netrw = 1 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrwPlugin = 1
 
-vim.g.gruvbox_material_foreground = 'original'
+vim.g.gruvbox_material_foreground = 'material'
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 vim.g.gruvbox_material_background = 'hard'
 
