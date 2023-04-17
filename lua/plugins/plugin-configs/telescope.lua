73ae3c7c8d688e0be314b29696a21a06eecb88ef
Emrozgs <emroz@gain.media>
2023-04-17 15:48:48 +0600

    Update

--- lua/plugins/plugin-configs/telescope.lua
+++ lua/plugins/plugin-configs/telescope.lua
@@ -3,5 +3,11 @@ if not telescope_status_ok then
 	return
 end
 
-telescope.setup()
+telescope.setup(
+    -- {
+    --     defaults = {
+    --         file_ignore_patterns = {'package', 'public/vendor', 'public/install', 'package/installer', 'composer.lock', 'package-lock.json', 'yarn.lock', }
+    --     }
+    -- }
+)
 
