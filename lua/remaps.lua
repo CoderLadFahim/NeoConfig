091b2cbc7b706b98142115597e76dfc28b6ae187
EmrozGS <emroz@gain.media>
2023-08-14 20:21:55 +0600

    Removed all icons

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -14,10 +14,10 @@ end
 
 function SEARCH_GIT_FILES()
 	if vim.fn.isdirectory('.git') == 1 then
-  		vim.cmd('Telescope git_files disable_devicons=false')
+  		vim.cmd('Telescope git_files disable_devicons=true')
 	else
 		print('Not a git repository, searching all files');
-  		vim.cmd('Telescope find_files disable_devicons=false')
+  		vim.cmd('Telescope find_files disable_devicons=true')
 	end
 end
 
@@ -45,9 +45,9 @@ end
 
 function VISUAL_GREP_STRING() 
     local visual_selection = GET_VISUAL_SELECTION()
-    print(visual_selection)
     require('telescope.builtin').grep_string({
-        search = visual_selection
+        search = visual_selection,
+        disable_devicons = true
     })
 end
 
@@ -89,14 +89,14 @@ set_keymap('n', '<leader>ty', ":lua OPEN_NVIM_TREE('right')<CR>")
 
 -- Telescope mappings
 set_keymap('n', "<leader>ff", ":lua SEARCH_GIT_FILES()<CR>")
-set_keymap('n', "<leader>FF", "<cmd>Telescope find_files disable_devicons=false<CR>")
-set_keymap('n', "<leader>fw", "<cmd>Telescope live_grep disable_devicons=false<CR>")
+set_keymap('n', "<leader>FF", "<cmd>Telescope find_files disable_devicons=true<CR>")
+set_keymap('n', "<leader>fw", "<cmd>Telescope live_grep disable_devicons=true<CR>")
 set_keymap('n', "<leader>gb", "<cmd>Telescope git_branches<CR>")
 set_keymap('n', "<leader>glo", "<cmd>Telescope git_commits<CR>")
 set_keymap('n', "<leader>gs", "<cmd>Telescope git_status<CR>")
 set_keymap('n', "<leader>hg", "<cmd>Telescope help_tags<CR>")
 set_keymap('n', "<leader>cs", "<cmd>Telescope colorscheme<CR>")
-set_keymap('n', "<leader>bf", "<cmd>Telescope buffers disable_devicons=false<CR>")
+set_keymap('n', "<leader>bf", "<cmd>Telescope buffers disable_devicons=true<CR>")
 set_keymap('n', "<leader>rm", "<cmd>Telescope keymaps<CR>")
 set_keymap('n', "<leader>cm", "<cmd>Telescope commands<CR>")
 set_keymap('n', "<leader>ds", "<cmd>Telescope lsp_document_symbols<CR>")
