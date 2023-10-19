17675fffe9f656ab8ea5235382c1b7303db15e87
fahim <fahim>
2023-10-19 06:09:13 +0600

    Fixed indent blankline and telescope

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -47,7 +47,7 @@ local plugins = {
      	end
   	},
 	{
-  	  	'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  	  	'nvim-telescope/telescope.nvim', tag = '0.1.4',
   	  	dependencies = { {'nvim-lua/plenary.nvim'} }
 	},
 	{
@@ -100,7 +100,7 @@ local plugins = {
 	{'akinsho/bufferline.nvim'},
 	'jose-elias-alvarez/null-ls.nvim',
 	'MunifTanjim/prettier.nvim',
-   	"lukas-reineke/indent-blankline.nvim",
+   	{"lukas-reineke/indent-blankline.nvim", commit = '9637670'},
    	"tpope/vim-fugitive",
 	"ggandor/leap.nvim",
 	-- "mrjones2014/nvim-ts-rainbow",
