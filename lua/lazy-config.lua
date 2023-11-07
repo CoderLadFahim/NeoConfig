2c215c5f46c4d3b3ab0f98a6fa58e751749bc7ce
EmrozGS <emroz@gain.media>
2023-11-07 13:48:58 +0600

    Cleaned up the lazy config file and disabled mouse support

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -1,10 +1,10 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" if not vim.loop.fs_stat(lazypath) then
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim' if not vim.loop.fs_stat(lazypath) then
   	vim.fn.system({
-    	"git",
-    	"clone",
-    	"--filter=blob:none",
-    	"https://github.com/folke/lazy.nvim.git",
-    	"--branch=stable", -- latest stable release
+    	'git',
+    	'clone',
+    	'--filter=blob:none',
+    	'https://github.com/folke/lazy.nvim.git',
+    	'--branch=stable', -- latest stable release
     	lazypath,
   	})
 end
@@ -16,64 +16,25 @@ local options = {
     },
     ui = {
         icons = {
-            cmd = "⌘",
-            config = "",
-            event = "",
-            ft = "",
-            init = "",
-            keys = "",
-            plugin = "",
-            runtime = "",
-            source = "",
-            start = "",
-            task = "",
-            lazy = " ",
+            cmd = '⌘',
+            config = '',
+            event = '',
+            ft = '',
+            init = '',
+            keys = '',
+            plugin = '',
+            runtime = '',
+            source = '',
+            start = '',
+            task = '',
+            lazy = ' ',
         }
     }
 }
 
 local plugins = {
   	'nvim-lua/plenary.nvim',
-  	-- 'MunifTanjim/nui.nvim',
-  	-- { 'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons', opt = false} },
-  	{
-  	  	"kylechui/nvim-surround",
-  	  	config = function()
-  	  	  	require("nvim-surround").setup({
-  	  	  		tag = "*", --  for stability; omit to use `main` branch for the latest features
-     		})
-     	end
-  	},
-	{
-  	  	'nvim-telescope/telescope.nvim', tag = '0.1.4',
-  	  	dependencies = { {'nvim-lua/plenary.nvim'} }
-	},
-	{
-    	'numToStr/Comment.nvim',
-    	config = function()
-        	require('Comment').setup()
-    	end
-	},
 	'JoosepAlviste/nvim-ts-context-commentstring',
-	{"akinsho/toggleterm.nvim", config = function()
-  	  	require("toggleterm").setup()
-		end
-	},
-	{
-		"windwp/nvim-autopairs",
-    	config = function() require("nvim-autopairs").setup {} end
-	},
-	{ 'lewis6991/gitsigns.nvim' },
-	-- {
- -- 	   	'goolord/alpha-nvim',
- -- 	   	config = function ()
- -- 	       	require'alpha'.setup(require'alpha.themes.dashboard'.config)
- -- 	   	end
-	-- },
-	{
-  	  	'nvim-tree/nvim-tree.lua',
-  	  	tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	},
 	'mg979/vim-visual-multi',
 	'hrsh7th/nvim-cmp',
 	'hrsh7th/cmp-buffer',
@@ -81,52 +42,32 @@ local plugins = {
 	'hrsh7th/cmp-cmdline',
 	'saadparwaiz1/cmp_luasnip',
 	'hrsh7th/cmp-nvim-lsp',
-	{"L3MON4D3/LuaSnip"},
+	'L3MON4D3/LuaSnip',
 	'rafamadriz/friendly-snippets',
-   	{
-      	'nvim-treesitter/nvim-treesitter',
-      	build = ':TSUpdate'
-   	},
-	-- 'nvim-treesitter/playground',
 	'neovim/nvim-lspconfig',
-	"williamboman/mason.nvim",
-    {
-        "glepnir/lspsaga.nvim",
-        event = "LspAttach",
-    }, 
-	-- {'akinsho/bufferline.nvim', dependencies = 'kyazdani42/nvim-web-devicons'},
-	-- {'akinsho/bufferline.nvim'},
+	'williamboman/mason.nvim',
 	'jose-elias-alvarez/null-ls.nvim',
 	'MunifTanjim/prettier.nvim',
-   	{"lukas-reineke/indent-blankline.nvim", commit = '9637670'},
-   	"tpope/vim-fugitive",
-	"ggandor/leap.nvim",
-	-- "mrjones2014/nvim-ts-rainbow",
-	-- ({
- --  	  	"utilyre/barbecue.nvim",
- --  	  	dependencies = {
- --    	 	"SmiteshP/nvim-navic",
- -- 			'kyazdani42/nvim-web-devicons', -- optional dependency
- --  	  	},
- --  	  	config = function()
- --    	 	require("barbecue").setup()
- --  	  	end,
-	-- }),
-    ({
-        "iamcco/markdown-preview.nvim",
-        build = function() vim.fn["mkdp#util#install"]() end,
-    }),
+   	'tpope/vim-fugitive',
+	'ggandor/leap.nvim',
     'jwalton512/vim-blade',
-    "sindrets/diffview.nvim",
-    --
+    'sindrets/diffview.nvim',
     'akinsho/git-conflict.nvim',
     'j-morano/buffer_manager.nvim',
-
-	-- "sainnhe/gruvbox-material",
-	-- { "ellisonleao/gruvbox.nvim" },
-	-- "sainnhe/sonokai",
-	-- "LunarVim/darkplus.nvim",
+	'lewis6991/gitsigns.nvim',
     'navarasu/onedark.nvim',
+
+  	{ 'kylechui/nvim-surround', config = function() require('nvim-surround').setup({ tag = '*', }) end },
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.4', dependencies = { {'nvim-lua/plenary.nvim'} } },
+	{ 'numToStr/Comment.nvim', config = function() require('Comment').setup() end },
+	{ 'akinsho/toggleterm.nvim', config = function() require('toggleterm').setup() end },
+	{ 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup {} end },
+	{ 'nvim-tree/nvim-tree.lua', tag = 'nightly' },
+   	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { 'glepnir/lspsaga.nvim', event = 'LspAttach' }, 
+   	{ 'lukas-reineke/indent-blankline.nvim', commit = '9637670'},
+    { 'iamcco/markdown-preview.nvim', build = function() vim.fn['mkdp#util#install']() end },
+
 }
 
 if ENABLE_ICONS() then
